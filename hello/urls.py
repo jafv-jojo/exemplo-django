5868e789d09f6e36fa2b563dbcec8e8f0b33b8be
jafv-jojo <jafv@cesar.school>
2026-09-02 11:17:45 -0300

    define as rotas da aplicacao

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib import admin
+
 
 urlpatterns = [
-    path("", views.index),
+    path("admin/", admin.site.urls),
+    path('hello/', include('hello.urls')),
+    path('forum/', include('forum.urls')),
+    path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'),  # Redirect root URL to forum
+
 ]
